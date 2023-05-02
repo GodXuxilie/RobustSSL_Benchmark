98596d110877d0b8645e57e9f67b558459f62217
XU XILIE <xuxilie@cgpa5.comp.nus.edu.sg>
2023-05-02 20:19:24 +0800

    update_run

--- run_tune_eval.sh
+++ run_tune_eval.sh
@@ -20,7 +20,7 @@ nohup python finetuning_eval.py --gpu 0 --experiment ACL_cifar10_r18_stl10 --dat
 nohup python finetuning_eval.py --gpu 2 --experiment DynACL_cifar10_r18_stl10 --dataset stl10 --pretraining DynACL --model r18 --checkpoint ./checkpoints/DynACL.pt --mode Ensemble --eval-AA &
 nohup python finetuning_eval.py --gpu 0 --experiment AdvCL_cifar10_r18_stl10 --dataset stl10 --pretraining AdvCL --model r18 --checkpoint ./checkpoints/AdvCL.pt --mode Ensemble --eval-AA &
 nohup python finetuning_eval.py --gpu 2 --experiment AInfoNCE_cifar10_r18_stl10 --dataset stl10 --pretraining A-InfoNCE --model r18 --checkpoint ./checkpoints/AInfoNCE.pt --mode Ensemble --eval-AA &
-nohup python finetuning_eval.py --gpu 2 --experiment DeACL_cifar10_r18_stl10 --dataset stl10 --pretraining DeACL --model r18 --checkpoint ./checkpoints/DeACL.pt --mode Ensemble --eval-AA --eval-OOD &
+nohup python finetuning_eval.py --gpu 2 --experiment DeACL_cifar10_r18_stl10 --dataset stl10 --pretraining DeACL --model r18 --checkpoint ./checkpoints/DeACL.pt --mode Ensemble --eval-AA &
 
 # transferability from CIFAR-100 to STL10
 nohup python finetuning_eval.py --gpu 1 --experiment ACL_cifar100_r18_stl10 --dataset stl10 --pretraining ACL --model r18 --checkpoint ./checkpoints/ACL_DS_CIFAR100.pt  --mode Ensemble &
