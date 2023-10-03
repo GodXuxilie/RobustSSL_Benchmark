b2eba89ac2721c8d8360a94e225749aa9fd5417f
XU XILIE <xuxilie@cgpa6.comp.nus.edu.sg>
2023-10-03 22:36:14 +0800

    update

--- Finetuning_Methods/Vanilla/run_vanilla_tune.sh
+++ Finetuning_Methods/Vanilla/run_vanilla_tune.sh
@@ -1,12 +1,12 @@
 # Transferabilit from CIFAR-10 to CIFAR-10
-nohup python vanilla_finetuning.py --gpu 3 --experiment ACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining ACL --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/dropbox_checkpoints/ACL.pt --mode AFF --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 3 --experiment DynACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/dropbox_checkpoints/DynACL.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 3 --experiment AdvCL_cifar10_r18_cifar10 --dataset cifar10 --pretraining AdvCL --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/AdvCL.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 2 --experiment AInfoNCE_cifar10_r18_cifar10 --dataset cifar10 --pretraining A-InfoNCE --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/AInfoNCE.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 2 --experiment DeACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining DeACL --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/DeACL.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL++_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL++ --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/DynACL++.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL_IR_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL_IR --model r18 --checkpoint /home/x/xuxilie/RobSSL_benchmark_copy/DynACL_IR.pt --mode ALL --eval-AA --eval-OOD &
-nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL++_IR_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL++_IR --model r18 --checkpoint ./home/x/xuxilie/RobSSL_benchmark_copy/DynACL++_IR.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 3 --experiment ACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining ACL --model r18 --checkpoint ./checkpoints/ACL.pt --mode AFF --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 3 --experiment DynACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL --model r18 --checkpoint ./checkpoints/DynACL.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 3 --experiment AdvCL_cifar10_r18_cifar10 --dataset cifar10 --pretraining AdvCL --model r18 --checkpoint ./checkpoints/AdvCL.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 2 --experiment AInfoNCE_cifar10_r18_cifar10 --dataset cifar10 --pretraining A-InfoNCE --model r18 --checkpoint ./checkpoints/AInfoNCE.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 2 --experiment DeACL_cifar10_r18_cifar10 --dataset cifar10 --pretraining DeACL --model r18 --checkpoint ./checkpoints/DeACL.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL++_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL++ --model r18 --checkpoint ./checkpoints/DynACL++.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL_IR_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL_IR --model r18 --checkpoint ./checkpoints/DynACL_IR.pt --mode ALL --eval-AA --eval-OOD &
+nohup python vanilla_finetuning.py --gpu 2 --experiment DynACL++_IR_cifar10_r18_cifar10 --dataset cifar10 --pretraining DynACL++_IR --model r18 --checkpoint ./checkpoints/DynACL++_IR.pt --mode ALL --eval-AA --eval-OOD &
 
 # Transferabilit from CIFAR-100 to CIFAR-100
 nohup python vanilla_finetuning.py --gpu 1 --experiment ACL_cifar100_r18_cifar100 --dataset cifar100 --pretraining ACL --model r18 --checkpoint ./checkpoints/ACL_DS_CIFAR100.pt  --mode ALL --eval-AA --eval-OOD &
